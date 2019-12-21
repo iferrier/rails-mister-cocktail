14ec5d2be782040a8b856930b7d8c73dbbd0703b
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-12-21 18:09:30 +0000

    Change back function create to save in cocktail controlleur

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,7 +13,7 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    if @cocktail.create
+    if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
       render :new
