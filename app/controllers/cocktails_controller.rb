80736c14f5c4cc7d40144a17bb183524ef6a910c
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-12-21 17:45:41 +0000

    Change params new cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,6 +29,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo, :photo_cache)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
